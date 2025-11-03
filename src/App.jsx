f14e41dd4c493a717d9a0d33de280399b6599e79
Hai Dang <dagger241004abc@gmail.com>
2025-11-03 22:06:11 +0700

    chore: update dependencies and add Tailwind CSS configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { useDispatch } from "react-redux";
-import { ToastContainer } from "react-toastify";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import AppRoutes from "@/components/AppRoutes";
 import ScrollTop from "./components/ScrollTop";
 import UserProvider from "./components/UserProvider";
 import { LoadingProvider } from "./context/LoadingContext";
-import { getCurrentUser } from "./features/auth/authAsync";
 
 function App() {
-  const dispatch = useDispatch();
-  dispatch(getCurrentUser());
-  dispatch(getCurrentUser());
-  dispatch(getCurrentUser());
-  dispatch(getCurrentUser());
-  dispatch(getCurrentUser());
-
   return (
     <LoadingProvider>
       <Router>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
 import App from "./App.jsx";
-import { persistor, store } from "./store/index.js";
+import { persistor, store } from "./store";
 
 import { createRoot } from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
 import { LoadingProvider } from "./context/LoadingContext.jsx";
 import { UserProvider } from "./context/UserContext.jsx";
 import { PersistGate } from "redux-persist/integration/react";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <LoadingProvider>
